0f0cdb443ee740e95f1d7ce18774dac02df971ee
CKDenni <deni3278@easv365.dk>
2022-05-04 10:20:47 +0200

    Cleanup code

--- acnh_mate_frontend/lib/Views/home_page.dart
+++ acnh_mate_frontend/lib/Views/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:acnh_mate_frontend/Widgets/drawer_button.dart';
 import 'package:flutter/material.dart';
 
-class home_page extends StatelessWidget {
-  const home_page({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +16,7 @@ class home_page extends StatelessWidget {
             child: ListView(
           children: <Widget>[
             DrawerHeader(
-                child: drawer_button(
+                child: DrawerButton(
               text: "Museum",
               onPressed: () {},
             )),
--- acnh_mate_frontend/lib/main.dart
+++ acnh_mate_frontend/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const home_page(),
+      home: const HomePage(),
     );
   }
 }
